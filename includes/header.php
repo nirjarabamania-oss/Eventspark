09e3ffbc7da71b50e364c2d5e5878df024ae7ba6
nirjarabamania-oss <nirjarabamania@gmail.com>
2026-08-07 09:03:29 +0530

    comman file added

---
+++ includes/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EventSpark</title>
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Your CSS -->
+    <link rel="stylesheet" href="../assets/css/style.css">
+</head>
+<body></body>
