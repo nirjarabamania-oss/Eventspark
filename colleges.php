57de15ae190d2b4f9e9d24ceac0770ebdebfbf17
nirjarabamania-oss <nirjarabamania@gmail.com>
2026-09-25 10:28:07 +0530

    college file change

--- colleges.php
+++ colleges.php
@@ -290,7 +290,6 @@ if (!$result) {
 
 <?php include("includes/footer.php"); ?>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
 </body>
 </html>
